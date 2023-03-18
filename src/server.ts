34cbaad90c32cbca94f800fe03d081519c72b8bc
VictorIShizuka <victorishizuka131@gmail.com>
2023-03-18 16:59:00 -0300

    .

--- src/server.ts
+++ src/server.ts
@@ -19,9 +19,7 @@ server.use(express.static(path.join(__dirname, '../public')));
 server.use(mainRoutes);
 
 server.use((req, res)=>{
-   res.send('pagina nao encotrada')
-
-   
+   res.send('pages/404');   
 })
 
 server.listen(process.env.PORT);
